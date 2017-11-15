61246dbc81a3437466b8f3b4c2b3837488c72019
xiaofzhang <2734951071@qq.com>
2017-11-15 17:41:10 +0800

    添加注释

--- src/main/java/Inherrance.java
+++ src/main/java/Inherrance.java
@@ -1,4 +1,8 @@
 public class Inherrance {
+    /**
+     * Java中创建子类对象时会默认调用父类中无参的构造函数，然后调用子类的构造函数。
+     * @param args
+     */
     public static void main(String[] args){
         ChildClass clazz = new ChildClass();
         ChildClass .staticMethod();
